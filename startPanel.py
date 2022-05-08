f068d29731cb3809b874f5d3e879bebfdc983905
MarioCrafter <berzelaiponce@gmail.com>
2022-05-07 19:58:16 -0700

    commit testing

--- startPanel.py
+++ startPanel.py
@@ -4,10 +4,11 @@ from pygame.locals import *
 class startPanel: 
     CYAN = (96, 168, 181)
     RED = (255,0,0)
+    BLUE = (0,0,255)
 
     def __init__(self, screen):
         self.running = True
-        self.background = self.CYAN
+        self.background = self.BLUE
         self.screen = screen
         self.screen.fill(self.background)
         pygame.display.update()
@@ -25,5 +26,4 @@ class startPanel:
                 if event.type == pygame.QUIT:
                     self.running = False
             self.screen.fill(self.background)
-            pygame.display.update()               
 
